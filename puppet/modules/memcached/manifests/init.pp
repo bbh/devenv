22a5f5a9396bd7f6f42db849d5d974505410bf11
Basilio Briceño <bbh@briceno.mx>
2013-10-01 21:47:12 -0700

    Memcache for ubuntu

--- puppet/modules/memcached/manifests/init.pp
+++ puppet/modules/memcached/manifests/init.pp
@@ -4,32 +4,19 @@ class memcached {
     ensure => installed,
   }
   file { 'config' :
-    path => '/etc/sysconfig/memcached',
+    path => '/etc/memcached.conf',
     mode => 0444,
     owner => root,
     group => root,
     ensure => present,
-    content => template('memcached/memcached_sysconfig.erb'),
+    content => template('memcached/memcached.conf.erb'),
     require => Package["memcached"],
   }
-  service { "memcached":
+  service { "memcached" :
     ensure => true,
     enable => true,
     hasstatus => true,
     hasrestart => true,
-    require => [File['config'],Package['memcached']],
-  }
-  file{ 'addport' :
-    path => '/tmp/addport.rb',
-    mode => 0550,
-    owner => root,
-    group => root,
-    ensure => present,
-    content => template('memcached/iptables_rb.erb'),
-    require => Service['memcached'],
-  }
-  exec { 'run_addport' :
-    command => '/tmp/addport.rb',
-    require => File['addport'],
+    require => File["config"],
   }
 }
